1ee70bed8672cf7681043db3d551f3274c236172
RohanRedekar <rohanredekar100@gmail.com>
2022-07-21 21:26:39 +0530

    added cors package

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-
+const cors = require("cors");
 const walletController = require("./src/controllers/wallet.controller");
 
-
+app.use(cors());
 app.get("", (req, res) => {
     try {
         res.send("homepage");
