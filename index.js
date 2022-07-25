6b37f0b812c146d90f4487694940dc6b0d46c62e
RohanRedekar <rohanredekar100@gmail.com>
2022-07-25 15:06:04 +0530

    product route added for single product

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 app.use(express.json());
 const cors = require("cors");
 const productsController = require("./src/controllers/products.controller");
+const productController = require("./src/controllers/product.controller");
 
 app.use(cors());
 app.get("", (req, res) => {
@@ -14,5 +15,6 @@ app.get("", (req, res) => {
 });
 
 app.use("/products/category", productsController);
+app.use("/product", productController)
 
 module.exports = app;
