9bcb3d8fb8e54ace5064be1468b65d9651d05ed1
RohanRedekar <rohanredekar100@gmail.com>
2022-07-26 22:25:04 +0530

    debugging changes

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,14 +1,15 @@
 const User = require("../models/user.model");
 
 const addToCart = (req, res, next) => {
+  const ip = req.ip;
   const user = User.findOne({ ip_adress: ip }).lean().exec();
   return res.status(201).send(user);
-    // .then((dbUser) => {
-    //   dbUser.addToCart(req.body.id);
-    // })
-    // .then(() => {
-    //   return res.status(201).send("added to cart successfully");
-    // })
+  // .then((dbUser) => {
+  //   dbUser.addToCart(req.body.id);
+  // })
+  // .then(() => {
+  //   return res.status(201).send("added to cart successfully");
+  // })
 };
 
 const getCart = (req, res, next) => {
