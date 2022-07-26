8fa576268d741641421717950c60a3fc0e7012d2
RohanRedekar <rohanredekar100@gmail.com>
2022-07-26 23:09:43 +0530

    storing cart for unique users didn't workout

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,33 +1,32 @@
-const User = require("../models/user.model");
+// const User = require("../models/user.model");
 
-const addToCart = (req, res, next) => {
-  const ip = new String(req.ip);
-  return res.status(201).send(ip);
-  // const user = User.findOne({ ip_adress: ip }).lean().exec();
-  // .then((dbUser) => {
-  //   dbUser.addToCart(req.body.id);
-  // })
-  // .then(() => {
-  //   return res.status(201).send("added to cart successfully");
-  // })
-};
+// const addToCart = (req, res, next) => {
+//   User.findOne({ ip_adress: ip })
+//     .then((dbUser) => {
+//       dbUser.addToCart(req.body.id);
+//     })
+//     .then(() => {
+//       return res.status(201).send("added to cart successfully");
+//     })
+//     .catch((err) => console.log(err));
+// };
 
-const getCart = (req, res, next) => {
-  req.user
-    .populate("cart.items.productId")
-    .lean()
-    .exec()
-    .then((user) => console.log(user))
-    .catch((err) => console.log(err));
-};
+// const getCart = (req, res, next) => {
+//   req.user
+//     .populate("cart.items.productId")
+//     .lean()
+//     .exec()
+//     .then((user) => console.log(user))
+//     .catch((err) => console.log(err));
+// };
 
-const deleteInCart = (req, res, next) => {
-  req.user
-    .removeFromCart(req.body.prodId)
-    .then(() => {
-      return res.status(201).send("removed from cart successfully");
-    })
-    .catch((err) => console.log(err));
-};
+// const deleteInCart = (req, res, next) => {
+//   req.user
+//     .removeFromCart(req.body.prodId)
+//     .then(() => {
+//       return res.status(201).send("removed from cart successfully");
+//     })
+//     .catch((err) => console.log(err));
+// };
 
-module.exports = { addToCart, getCart, deleteInCart };
+// module.exports = { addToCart, getCart, deleteInCart };
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,20 +1,20 @@
-const User = require("../models/user.model");
+// const User = require("../models/user.model");
 
-const register = async (req, res) => {
-  try {
-    const ip = req.ip;
-    let user = await User.findOne({ ip_adress: ip });
+// const register = async (req, res) => {
+//   try {
+//     const ip = req.ip;
+//     let user = await User.findOne({ ip_adress: ip });
 
-    if (user) {
-      return res.status(500).send("User already exists");
-    }
-    req.body.ip_adress = ip;
-    user = await User.create(req.body);
+//     if (user) {
+//       return res.status(500).send("User already exists");
+//     }
+//     req.body.ip_adress = ip;
+//     user = await User.create(req.body);
 
-    return res.status(200).send(ip);
-  } catch (err) {
-    res.status(400).send({ message: err.message });
-  }
-};
+//     return res.status(200).send(ip);
+//   } catch (err) {
+//     res.status(400).send({ message: err.message });
+//   }
+// };
 
-module.exports = register;
+// module.exports = register;
