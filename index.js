d40180f07ab5a91b3715d5f2dfe9e58a492f84c4
RohanRedekar <rohanredekar100@gmail.com>
2022-07-27 13:45:41 +0530

    cors middleware was not working so added access through access acontrol

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const cors = require("cors");
+app.use(cors());
 const productsController = require("./src/controllers/products.controller");
 const productController = require("./src/controllers/product.controller");
 
-app.use(cors());
+
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 app.get("", (req, res) => {
   try {
